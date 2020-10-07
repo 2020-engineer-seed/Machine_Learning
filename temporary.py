1a604680f5a41a67a5f599888bc3908c096732dd
Cocona Matsuda <skyebell13rei@gmail.com>
2020-10-08 03:25:06 +0900

    ロードまで...

--- temporary.py
+++ temporary.py
@@ -1,15 +1,14 @@
+# ライブラリをロード
 import pandas as pd
-import numpy as np
-from sklearn.modenl_selection import train_test_split
+from sklearn import datasets
+from sklearn.neighbors import NearestNeighbors
+from sklearn.preprocessing import StandardScaler
 
-#データ読み込み
-set_deta = pd.read_csv('DataSet_2020_09_18_16_46_43.csv',encoding="SHIFT-JIS")
+# URLを作成
+url = 'https://github.com/2020-engineer-seed/datasets/blob/master/Dataset_learn_1.csv'
 
-#データをラベルと入力データに分離する
+# データセットをロード
+dataframe = pd.read_csv(url)
 
-#学習用とテスト用に分離する
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, train_size = 0.8, shuffle =True)
-
-#学習する
-
-#評価する
+# 標準化器を作成
+starndardizer = StandardScaler()
